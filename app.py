8ef9eab9cc9f53a8f2d3c1185610264098912665
rishabhc9 <61555936+rishabhc9@users.noreply.github.com>
2021-09-14 18:10:04 +0530

    abc

--- app.py
+++ app.py
@@ -48,8 +48,55 @@ def sparkfun(url):
         result = df.to_json(orient="index")
         parsed = json.loads(result)
         h=json.dumps(parsed, indent=4)
-        return f'<h5>{h}</h5>'
+        return h
 
+@app.route('/productdetails/<url1>',  methods=["GET", "POST"])
+def sparkfun2(url1):
+    uri=str(url1)
+    compurl="https://www.sparkfun.com/products/"+uri
+    result = requests.get(compurl)
+    src = result.content
+    soup = BeautifulSoup(src, 'lxml')
+    prodesc=[]
+    products = soup.find_all('div', attrs={'id': 'description-tab'})
+    for u in products:
+            for v in u.find_all('p'):
+                    prodesc.append(v.text)
+    
+    prodprice=[]
+    pprices = soup.find_all('div', attrs={'class': 'display-price'})
+    for pp in pprices:
+            for tt in pp.find_all('h3'):
+                    oo=tt.text
+    prodprice.append(oo)
+
+    prodtitle=[]
+    ptitle = soup.find_all('div', attrs={'class': 'product-title'})
+    for pt in ptitle:
+            for title in pt.find_all('h1'):
+                oott=title.text
+    prodtitle.append(oott)
+
+    aggrating=[]
+    rating = soup.find_all('div', attrs={'itemprop': 'aggregateRating'})
+    for g in rating:
+            for h in g.find_all('h3'):
+                    aggrating.append(h.text)
+
+    reviewdes=[]
+    reviews = soup.find_all('div', attrs={'class': 'review-text'})
+    for a in reviews:
+            for f in a.find_all('p'):
+                reviewdes.append(f.text)
+    
+    reviewperson=[]
+    author = soup.find_all('p', attrs={'class': 'review-byline text-muted'})
+    for au in author:
+            for auid in au.find_all('a'):
+                    reviewperson.append(auid['href'])
+    
+    proddata={'Product Name':prodtitle,'Product Description':prodesc,'Product Price':prodprice,'Product Rating':aggrating,"Produc Reviews":reviewdes,"Reviewed By":reviewperson}
+    return proddata
 
 if __name__ == "__main__":
     app.run(debug=True)
